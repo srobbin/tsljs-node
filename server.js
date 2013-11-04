8d5b32ae93a4ad2f9131782063fe8c9af0eb34bb
Scott Robbin <scott@robbin.co>
2013-11-04 10:52:01 -0600

    Socket.io

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
-var express = require("express");
-var app = express();
-app.use(express.logger());
+var express = require('express')
+  , app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+//CORS middleware
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+}
+ 
+app.configure(function () {
+  app.use(allowCrossDomain);
+  app.use(express.bodyParser());
+});
+
+/* Site */
+app.get('/', function(req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+app.post('/', function (req, res) {
+  io.sockets.emit('message', { message: req.body.message });
+  res.end(req.body.message);
+});
+
+/* Socket */
+io.sockets.on('connection', function (socket) {
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
+server.listen(port, function() {
   console.log("Listening on " + port);
 });
+
